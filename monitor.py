985fab866670e0b1160595a7c350699431f0e6e1
Andre Muzy <60109618+avmuzy@users.noreply.github.com>
2022-03-20 23:59:04 -0300

    add button

---
+++ monitor.py
@@ -0,0 +1,18 @@
+import tkinter
+from tkinter import *
+root = Tk()
+
+root.geometry(newGeometry=str('300x400'))
+root.title('Monitor')
+text = Label (root, text='System Monitor')
+text.pack()
+temp = Button(root, text='temperature')
+temp.pack()
+
+
+
+
+
+
+
+root.mainloop()
