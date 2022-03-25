dd1289250cecb0a3ae3af3e5c3821d98bd150a01
Andre Muzy <60109618+avmuzy@users.noreply.github.com>
2022-03-25 18:29:07 -0300

    placing action into the buttons

--- monitor.py
+++ monitor.py
@@ -2,37 +2,41 @@ import tkinter
 import psutil
 
 
-print('The cpu usage is: ', psutil.cpu_percent(4))
-print('RAM memory % used is:' , psutil.virtual_memory()[2])
+
+print('RAM memory % used is:', psutil.virtual_memory()[2])
 print('CPU freq:', psutil.cpu_freq()[0])
 print('Disk usage:', psutil.disk_usage('/')[3])
-print('swap memory:', psutil.swap_memory()[3])
+print('Swap memory:', psutil.swap_memory()[3])
 
 from tkinter import *
 root = Tk()
 
 root.geometry(newGeometry=str('300x400'))
 root.title('Monitor')
-text = Label (root, text='System Monitor')
+text = Label(root, text='System Monitor')
 text.pack()
 
-temp = Button(root, text='temperature')
-temp.pack()
 
-fan = Button(root, text='Fan speed')
-fan.pack()
+def cpu():
+    print('The cpu usage is: ', psutil.cpu_percent(4))
 
-net = Button(root, text='Network')
-net.pack()
 
-cpu = Button(root, text='CPU')
+def disk():
+    print('Disk usage:', psutil.disk_usage('/')[3])
+
+
+def swapmem():
+    print('Swap memory:', psutil.swap_memory()[3])
+
+
+cpu = Button(root, text='CPU', command=cpu)
 cpu.pack()
 
-disk = Button(root, text='Disk')
+disk = Button(root, text='Disk', command=disk)
 disk.pack()
 
-mem = Button(root, text='Memory')
-mem.pack()
+swapmem = Button(root, text='Swap Memory', command=swapmem)
+swapmem.pack()
 
 
 root.mainloop()
