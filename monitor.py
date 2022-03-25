0d0daf82f882fcfb3af786855727e8fad718afbf
Andre Muzy <60109618+avmuzy@users.noreply.github.com>
2022-03-25 14:57:23 -0300

    name fixing

--- monitor.py
+++ monitor.py
@@ -6,7 +6,7 @@ print('The cpu usage is: ', psutil.cpu_percent(4))
 print('RAM memory % used is:' , psutil.virtual_memory()[2])
 print('CPU freq:', psutil.cpu_freq()[0])
 print('Disk usage:', psutil.disk_usage('/')[3])
-print('swap memory:', psutil.swap_memory())
+print('swap memory:', psutil.swap_memory()[3])
 
 from tkinter import *
 root = Tk()
@@ -31,7 +31,7 @@ cpu.pack()
 disk = Button(root, text='Disk')
 disk.pack()
 
-mem = Button(root, text='Mamory')
+mem = Button(root, text='Memory')
 mem.pack()
 
 
