ef2864bb6f4b8cfb313989c9e260cd0ad1a961ce
Andre Muzy <60109618+avmuzy@users.noreply.github.com>
2022-03-26 11:39:19 -0300

    Ubuntu Monitor

---
+++ monitor-ubuntu.py
@@ -0,0 +1,67 @@
+import tkinter
+import psutil
+
+
+
+
+
+
+
+
+from tkinter import *
+root = Tk()
+
+root.geometry(newGeometry=str('300x300'))
+root.title('Monitor')
+text = Label(root, text='System Monitor')
+text.pack()
+
+
+def cpu():
+    print('The cpu usage is: ', psutil.cpu_percent(4))
+
+
+def fre():
+    print('CPU freq:', psutil.cpu_freq()[0])
+
+
+def ram():
+    print('RAM memory % used is:', psutil.virtual_memory()[2])
+
+
+def disk():
+    print('Disk usage:', psutil.disk_usage('/')[3])
+
+
+def swap():
+    print('Swap memory:', psutil.swap_memory()[3])
+
+
+def sens():
+    print('Sensors Temperature', psutil.sensors_temperatures())
+
+
+def battery():
+    print('Battery:', psutil.sensors_battery()[0])
+
+
+cpu = Button(root, text='CPU', command=cpu)
+cpu.pack()
+
+fre = Button(root, text='CPU Frequency', command=fre)
+fre.pack()
+
+ram = Button(root, text='Ram Memory', command=ram)
+ram.pack()
+
+disk = Button(root, text='Disk usage',command=disk)
+disk.pack()
+
+temp = Button(root, text='Temperature', command=sens)
+temp.pack()
+
+battery = Button(root, text='Battery', command=battery)
+battery.pack()
+
+root.mainloop()
+
