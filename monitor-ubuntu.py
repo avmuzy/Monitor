14f14b9ca482aa2657d9f81a953ceb626a86c48b
avms <andrevmuzy@gmail.com>
2022-03-28 16:51:35 -0300

    adding messagebox

--- monitor-ubuntu.py
+++ monitor-ubuntu.py
@@ -1,14 +1,9 @@
 import tkinter
 import psutil
 
-
-
-
-
-
-
-
 from tkinter import *
+from tkinter import messagebox
+
 root = Tk()
 
 root.geometry(newGeometry=str('300x300'))
@@ -17,32 +12,34 @@ text = Label(root, text='System Monitor')
 text.pack()
 
 
+
 def cpu():
-    print('The cpu usage is: ', psutil.cpu_percent(4))
+    messagebox.showinfo('The CPU usage is:', psutil.cpu_percent(4))
 
 
 def fre():
     print('CPU freq:', psutil.cpu_freq()[0])
-
+    messagebox.showinfo('CPU freq:', psutil.cpu_freq()[0])
 
 def ram():
     print('RAM memory % used is:', psutil.virtual_memory()[2])
-
+    messagebox.showinfo('RAM memory % used is:', psutil.virtual_memory()[2])
 
 def disk():
     print('Disk usage:', psutil.disk_usage('/')[3])
-
+    messagebox.showinfo('Disk usage:', psutil.disk_usage('/')[3])
 
 def swap():
     print('Swap memory:', psutil.swap_memory()[3])
-
+    messagebox.showinfo('Swap memory:', psutil.swap_memory()[3])
 
 def sens():
-    print('Sensors Temperature', psutil.psutil.sensors_temperatures()['acpitz'][0])
-
+    print('Sensors Temperature', psutil.sensors_temperatures()['acpitz'][0])
+    messagebox.showinfo('Sensors Temperature', psutil.sensors_temperatures()['acpitz'][0])
 
 def battery():
     print('Battery:', psutil.sensors_battery()[0])
+    messagebox.showinfo('Battery:', psutil.sensors_battery()[0])
 
 
 cpu = Button(root, text='CPU', command=cpu)
@@ -54,7 +51,7 @@ fre.pack()
 ram = Button(root, text='Ram Memory', command=ram)
 ram.pack()
 
-disk = Button(root, text='Disk usage',command=disk)
+disk = Button(root, text='Disk usage', command=disk)
 disk.pack()
 
 temp = Button(root, text='Temperature', command=sens)
@@ -64,4 +61,3 @@ battery = Button(root, text='Battery', command=battery)
 battery.pack()
 
 root.mainloop()
-
